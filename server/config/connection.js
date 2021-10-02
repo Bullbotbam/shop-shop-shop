71852c8b720d92b737cacca7c867465b2dd285bf
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-02 02:16:42 -0500

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-	process.env.MONGODB_URI || 'mongodb://localhost/shop-shop-shop',
+	process.env.MONGODB_URI || 'mongodb://localhost/mernshopping',
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
